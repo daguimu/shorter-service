a4a2d5bcf8a6dc999cd5943c1c19cb81ea6574c7
daguimu <1712066621@qq.com>
2020-01-09 17:03:52 +0800

    添加日志并优化

--- src/main/java/com/dagm/shorter/service/impl/ShorterRecordServiceImpl.java
+++ src/main/java/com/dagm/shorter/service/impl/ShorterRecordServiceImpl.java
@@ -61,7 +61,9 @@ public class ShorterRecordServiceImpl implements ShorterRecordService {
         if (recordDto == null
             && (recordDto = ShorterAdapter
             .shortPo2Dto(shorterRecordMapper.getOssFilePathById(tableName, leafId))) != null) {
-            redisStoreClient.set(setting.getKey(), recordDto, 5 * 60);
+            boolean aBoolean = redisStoreClient.set(setting.getKey(), recordDto, 5 * 60);
+            log.info("从db中查询到recordDto:[{}],succ:[{}]", recordDto, aBoolean);
+
         }
         boolean result = true;
         //记录不存在或者已过期

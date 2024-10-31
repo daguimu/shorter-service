bdadfb5f936328632d0c5a6886d6e411fe2289b4
daguimu <daguimu@keep.com>
2024-10-31 18:38:53 +0800

    feat: done

--- src/main/java/com/dagm/shorter/ShorterApplication.java
+++ src/main/java/com/dagm/shorter/ShorterApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author guimu
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients

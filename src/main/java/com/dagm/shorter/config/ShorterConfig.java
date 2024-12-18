1faaa24f9b60b3caa2d615cb3110b66f69502219
daguimu <daguimu@keep.com>
2024-12-18 09:25:12 +0800

    feat: add log

--- src/main/java/com/dagm/shorter/config/ShorterConfig.java
+++ src/main/java/com/dagm/shorter/config/ShorterConfig.java
@@ -7,8 +7,12 @@ package com.dagm.shorter.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author Guimu
@@ -18,14 +22,25 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "spring.shorter")
 @Getter
 @Setter
+@Slf4j
 public class ShorterConfig {
 
-  private String endpoint;
-  private String accessKeyId;
-  private String accessKeySecret;
-  private String bucketName;
-  private String bitStr;
-  private String defaultUrl;
-  private String baseUrl;
-  private String ossSuffix;
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String bucketName;
+    private String bitStr;
+    private String defaultUrl;
+    private String baseUrl;
+    private String ossSuffix;
+
+    @Value("${nacos.username}")
+    private String uname;
+    @Value("${nacos.password}")
+    private String passwd;
+
+    @PostConstruct
+    public void log() {
+        log.info("nacos uname is: {}, passwd is: {}", uname, passwd);
+    }
 }

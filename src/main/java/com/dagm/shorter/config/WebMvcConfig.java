44903fa6d976c031b47a1aaaf78e81916e61d63b
daguimu <daguimu@keep.com>
2025-01-15 21:14:59 +0800

    feat: fixed utf8

--- src/main/java/com/dagm/shorter/config/WebMvcConfig.java
+++ src/main/java/com/dagm/shorter/config/WebMvcConfig.java
@@ -3,8 +3,13 @@ package com.dagm.shorter.config;
 import com.dagm.shorter.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @desc 对requestMapping方法进行拦截, 除了登陆方法
@@ -21,5 +26,10 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
         super.addInterceptors(registry);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
     }
 }

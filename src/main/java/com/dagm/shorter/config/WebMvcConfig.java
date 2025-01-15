b5722a844a815ea9ae4340f4290cf332124302a5
daguimu <daguimu@keep.com>
2025-01-15 21:24:21 +0800

    feat: fixed utf8

--- src/main/java/com/dagm/shorter/config/WebMvcConfig.java
+++ src/main/java/com/dagm/shorter/config/WebMvcConfig.java
@@ -2,18 +2,17 @@ package com.dagm.shorter.config;
 
 import com.dagm.shorter.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import javax.servlet.Filter;
 
 /**
- * @desc 对requestMapping方法进行拦截, 除了登陆方法
  * @author Guimu
+ * @desc 对requestMapping方法进行拦截, 除了登陆方法
  * @create 2018/04/18 18:52:16
  **/
 @Configuration
@@ -28,8 +27,12 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         super.addInterceptors(registry);
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
+
 }

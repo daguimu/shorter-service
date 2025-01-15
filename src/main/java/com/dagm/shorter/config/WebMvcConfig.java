dbee4c4052f330990fc4b24102106500eb99402b
daguimu <daguimu@keep.com>
2025-01-15 21:46:00 +0800

    feat: fixed utf8

--- src/main/java/com/dagm/shorter/config/WebMvcConfig.java
+++ src/main/java/com/dagm/shorter/config/WebMvcConfig.java
@@ -3,14 +3,8 @@ package com.dagm.shorter.config;
 import com.dagm.shorter.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 /**
  * @author Guimu
@@ -27,20 +21,5 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
         super.addInterceptors(registry);
-    }
-
-    @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        for (HttpMessageConverter<?> converter : converters) {
-            // 解决 Controller 返回普通文本中文乱码问题
-            if (converter instanceof StringHttpMessageConverter) {
-                ((StringHttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8);
-            }
-
-            // 解决 Controller 返回json对象中文乱码问题
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                ((MappingJackson2HttpMessageConverter) converter).setDefaultCharset(StandardCharsets.UTF_8);
-            }
-        }
     }
 }
